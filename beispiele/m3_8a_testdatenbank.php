9e3ebedb8230be6dfc3ed7b3d0c90b7290b8f2ee
Adilaour <adil@420blaze.it>
2020-11-18 16:43:30 +0100

    Meilenstein 3 Aufgabe 6 fertig

---
+++ beispiele/m3_8a_testdatenbank.php
@@ -0,0 +1,76 @@
+<?php
+
+$link=mysqli_connect("localhost",      // Host der Datenbank
+    "root",                            // Benutzername zur Anmeldung
+    "abc123",                      // Passwort
+    "emensawerbeseite"            // Auswahl der Datenbanken (bzw. des Schemas)
+// optional port der Datenbank
+);
+if (!$link) {
+    echo "Verbindung fehlgeschlagen: ", mysqli_connect_error();
+    exit();
+}
+$sql = "SELECT id, name, beschreibung FROM gericht ORDER BY name LIMIT 5;";
+$result = mysqli_query($link,$sql);
+if(!$result){
+    echo "Fehler bei der Abfrage: ", mysqli_error($link);
+    exit();
+}
+echo '<ul>';
+while($row = mysqli_fetch_assoc($result)){
+    echo '<li>','<a href="rezept.php?id='.$row['id'].'">',$row['name'],'</a>',' - ',$row['beschreibung'],'</li>';
+    // var_dump($row);
+}
+echo '</ul>';
+mysqli_free_result($result);
+mysqli_close($link);
+
+$link = mysqli_connect("localhost","root", "abc123", "emensawerbeseite");
+if (!$link) {
+    echo "Verbindung fehlgeschlagen: ", mysqli_connect_error();
+    exit();
+}
+$sql = "SELECT a.name AS Allergen, GROUP_CONCAT(g.name) AS Gerichtname FROM gericht g LEFT JOIN gericht_hat_allergen gha ON gha.gericht_id = g.id RIGHT JOIN allergen a ON gha.code = a.code GROUP BY a.name;";
+$result = mysqli_query($link, $sql);
+if(!$result){
+    echo "Fehler bei der Abfrage: ", mysqli_error($link);
+    exit();
+}
+echo '<h5>OBEN: Übung</h5><h1>UNTEN: Aufgabe 6</h1>'.'<ul>';
+while($row = mysqli_fetch_assoc($result)){
+    echo '<li>'.$row['Allergen'].'<br>'.$row['Gerichtname'].'</li>';
+}
+echo '</ul>';
+mysqli_free_result($result);
+mysqli_close($link);
+
+
+// connect_example.php
+/*
+$link = mysqli_connect("localhost", // Host der Datenbank
+    "root",                 // Benutzername zur Anmeldung
+    "abc123",               // Passwort
+    "emensawerbeseite",     // Auswahl der Datenbanken (bzw. des Schemas)
+    "3306"                 // optional port der Datenbank
+);
+
+if (!$link) {
+    echo "Verbindung fehlgeschlagen: ", mysqli_connect_error();
+    exit();
+}
+
+$sql = "SELECT id, name, beschreibung FROM gericht";
+
+$result = mysqli_query($link, $sql);
+if (!$result) {
+    echo "Fehler während der Abfrage:  ", mysqli_error($link);
+    exit();
+}
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo '<li>', $row['id'], ':', $row['name'], '</li>';
+}
+
+mysqli_free_result($result);
+mysqli_close($link);
+*/
--- werbeseite/gerichte.php
+++ werbeseite/gerichte.php
@@ -21,9 +21,9 @@ $gerichte = [
         'name'        => 'Spaghetti Bolognese',
         'preisintern' => '1,50',
         'preisextern' => '6,20'],
-    4 => [
+    /*4 => [
         'bildname'    => 'jaegerschnitzel.jpg',
         'name'        => 'Jägerschnitzel mit Pommes',
         'preisintern' => '1,50',
-        'preisextern' => '6,20']
+        'preisextern' => '6,20']*/
 ];
