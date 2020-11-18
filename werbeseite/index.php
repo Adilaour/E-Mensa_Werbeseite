c960ff0962723976a48856151cb2e074e4583986
Adilaour <adil@420blaze.it>
2020-11-18 17:39:46 +0100

    Meilenstein 3 Aufgabe 6 fertig

--- werbeseite/index.php
+++ werbeseite/index.php
@@ -86,10 +86,28 @@
             </tr>
             <?php
             $gerichtanzahl = 0;
+            $link = mysqli_connect("localhost", "root","abc123","emensawerbeseite");
+            if (!$link) {
+                echo "Die Verbindung zur Datenbank ist fehlgeschlagen: ", mysqli_connect_error();
+                exit();
+            }
+            $sql="SELECT name, preis_intern, preis_extern FROM gericht ORDER BY name LIMIT 5";
+            $result = mysqli_query($link, $sql);
+            if(!$result){
+                echo "Fehler bei der Abfrage: ", mysqli_error($link);
+                exit();
+            }
+            while($row = mysqli_fetch_assoc($result)){
+                echo '<tr><td><img src="img/" alt="Speisebild nicht gefunden.">'.'</td><td class="alignleft">'.$row['name'].'</td><td>'.$row['preis_intern'].'</td><td>'.$row['preis_extern'].'</td></tr>';
+                $gerichtanzahl++;
+            }
+            mysqli_free_result($result);
+            mysqli_close($link);
+            /*
             foreach($gerichte as $value){
                 echo "<tr><td><img src='img/$value[bildname]' alt='Speisebild nicht gefunden.'></td><td class='alignleft'>$value[name]</td><td>$value[preisintern]</td><td>$value[preisextern]</td></tr>";
                 $gerichtanzahl++;
-            }
+            }*/
 
             ?>
         </table>
