6d4a0474ffe59a6e0d75d2b43920d3996d189cf8
Adilaour <adil@420blaze.it>
2020-12-02 18:42:00 +0100

    Meilenstein4 Aufgabe1.3/1.5/1.6

--- beispiele/m3_8a_testdatenbank.php
+++ beispiele/m3_8a_testdatenbank.php
@@ -2,7 +2,7 @@
 
 $link=mysqli_connect("localhost",      // Host der Datenbank
     "root",                            // Benutzername zur Anmeldung
-    "DBwt",                      // Passwort
+    "abc123",                      // Passwort
     "emensawerbeseite"            // Auswahl der Datenbanken (bzw. des Schemas)
 // optional port der Datenbank
 );
--- werbeseite/wunschgericht.php
+++ werbeseite/wunschgericht.php
@@ -1,40 +1,71 @@
 <?php
 $gerichtanzahl = 0;
-$link = mysqli_connect("localhost", "root","DBwt","emensawerbeseite");
-
-if (!$link) {
-    echo "Die Verbindung zur Datenbank ist fehlgeschlagen: ", mysqli_connect_error();
-    exit();
+// Variablen werden gesetzt
+if(isset($_POST["wunschgericht_name"])){
+    $wunschgericht_name = $_POST["wunschgericht_name"];
 }
-$wunschgericht_name = $_POST["wunschgericht_name"];
-$beschreibung = $_POST["beschreibung"];
-$name = $_POST["name"];
-$email = $_POST["email"];
+if(isset($_POST["beschreibung"])){
+    $beschreibung = $_POST["beschreibung"];
+}
+if(isset($_POST["name"])){
+    $name = $_POST["name"];
+}else{
+    $name= "";
+}
+if(isset($_POST["email"])){
+    $email = $_POST["email"];
+}
 
-$sql = "INSERT INTO wunschgerichte (name, beschreibung, erstellungsdatum, erstellerInnen) VALUE '$wunschgericht_name', '$beschreibung', '$name','$email'";
+// Datenbank Abfrage
+if(isset($_POST["wunschgericht_name"]) && isset($_POST["beschreibung"]) && isset($_POST["email"])){
+    // SQL Statement zusammenbasteln
+    $sql = "INSERT INTO wunschgerichte (name, beschreibung, erstellungsdatum, erstellerInnen) VALUE '$wunschgericht_name', '$beschreibung', '$name','$email'";
 
-mysqli_close($link);
+
+
+
+
+
+
+
+
+    // Verbindung zur Datenbank aufbauen und überprüfen
+    $link = mysqli_connect("localhost", "root","abc123","emensawerbeseite");
+    if (!$link) {
+        echo "Die Verbindung zur Datenbank ist fehlgeschlagen: ", mysqli_connect_error();
+        exit();
+    }
+    // Datenbank abfragen & Ergebnis prüfen
+    $result = mysqli_query($link,$sql);
+    if(!$result){
+        echo "Fehler bei der Abfrage: ", mysqli_error($link);
+        exit();
+    }
+    // Datenbankverbindung wird geschlossen
+    mysqli_close($link);
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8"/>
-    <title></title>
+    <title>Wunschgericht Formular</title>
 </head>
 <body>
-
-<form action="" method="post">
+<form action="wunschgericht.php" method="post">
     <label>Wunschgericht:
-        <input type="text" name="wunschgericht_name" id="wunschgericht_name">
+        <input type="text" name="wunschgericht_name" id="wunschgericht_name" required>
     </label>
     <label>Beschreibung:
-        <textarea name="beschreibung" id="beschreibung"></textarea>
+        <textarea name="beschreibung" id="beschreibung" required></textarea>
     </label>
-    <label>Name ErstellerIn:
+    <label>Name:
         <input type="text" name="name" id="name">
-        <label>E-Mail:
-            <input type="email" name="email" id="email">
+    </label>
+    <label>E-Mail:
+            <input type="email" name="email" id="email" required>
     </label>
     <input type="submit" value="speichern">
 </form>
